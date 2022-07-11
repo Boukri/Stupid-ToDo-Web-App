ed1ef4fa0a8c4fcf6701f8e8f6be9e0939fb2281
Abdelkhalek <boukri.a.khalek@gmail.com>
2022-07-11 17:26:40 +0200

    first commit

---
+++ FirstMVC/Controllers/TaskController.cs
@@ -0,0 +1,30 @@
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVC.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static List<TaskModel> Tasks = new List<TaskModel>();
+        public IActionResult Index()
+        {
+            return View(Tasks);
+        }
+
+        public IActionResult Create()
+        {
+            var task = new TaskModel();
+            return View(task);
+        }
+
+        public IActionResult CreateTask(TaskModel _task)
+        {
+            Tasks.Add(_task);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ FirstMVC/Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVC.Models
+{
+    public class TaskModel
+    {
+        public string Name { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
